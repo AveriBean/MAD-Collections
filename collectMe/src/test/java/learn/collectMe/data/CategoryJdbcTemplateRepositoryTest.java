0afaeb92fa7e934d7cdccdbf6603e6282f598bcb
Dwight C Kornbluth II <dkornbluth@dev-10.com>
2023-03-28 16:04:56 -0400

    get testfor Category started

---
+++ collectMe/src/test/java/learn/collectMe/data/CategoryJdbcTemplateRepositoryTest.java
@@ -0,0 +1,7 @@
+package learn.collectMe.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryJdbcTemplateRepositoryTest {
+
+}
---
+++ collectMe/src/test/java/learn/collectMe/data/KnownGoodState.java
@@ -0,0 +1,2 @@
+package learn.collectMe.data;public class KnownGoodState {
+}
