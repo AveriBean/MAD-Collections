bb1dd03bf187ed85e0d5d7bdbe0c66a4da393ee5
mgharder <mharder@dev-10.com>
2023-03-28 09:55:10 -0500

    schema

--- collect_me_schema.sql
+++ collect_me_schema.sql
@@ -2,10 +2,24 @@ drop database if exists collect_me;
 create database collect_me;
 use collect_me;
 
+create table user (
+user_id int primary key auto_increment,
+first_name varchar(64) not null,
+last_name varchar(64) not null,
+location varchar(255) null,
+password_hash varchar(1028) not null,
+phone varchar(64) null,
+email varchar(64) not null
+);
+
 create table item (
 item_id int primary key auto_increment,
 `name` varchar(30) not null,
-description varchar(128) not null
+description varchar(128) not null,
+user_id int not null,
+constraint fk_item_user_id
+        foreign key (user_id)
+       references user(user_id)
 );
 
 create table `action` (
@@ -31,13 +45,58 @@ constraint fk_item_action_item_id
   
 );
 
-create table user (
-user_id int primary key auto_increment,
-first_name varchar(64),
-last_name varchar(64),
-location varchar(255) ,
-password_hash varchar(1028) not null,
-phone varchar(64) null,
-email varchar(64) not null,
+
+create table role (
+role_id int primary key auto_increment,
+role_name varchar(32) not null
 );
+
+create table user_role (
+    user_id int not null,
+    role_id int not null,
+    constraint pk_user_role
+        primary key (user_id, role_id),
+    constraint fk_user_role_user_id
+        foreign key (user_id)
+        references user(user_id),
+    constraint fk_user_role_role_id
+        foreign key (role_id)
+        references role(role_id)
+);
+
+create table category (
+category_id int primary key,
+`name` varchar(64) not null
+);
+
+create table category_item (
+category_id int not null,
+item_id int not null,
+constraint pk_category_item
+primary key (category_id, item_id),
+constraint fk_category_item_category_id
+foreign key (category_id)
+references category(category_id),
+constraint fk_category_item_item_id
+foreign key (item_id)
+references item(item_id)
+);
+
+create table comment (
+comment_id int primary key auto_increment,
+user_id int not null,
+item_id int not null,
+content varchar(1028) not null,
+constraint fk_comment_user_id
+foreign key (user_id)
+references user(user_id),
+constraint fk_comment_item_id
+foreign key (item_id)
+references item(item_id)
+);
+
+
+
+
+
 
