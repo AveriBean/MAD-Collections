af125a5902b6e8294a7b27f36b0acabeb3ae1638
Dwight C Kornbluth II <dkornbluth@dev-10.com>
2023-03-28 16:11:23 -0400

    updated application.properties for both production and test

--- collectMe/src/test/java/learn/collectMe/data/CategoryJdbcTemplateRepositoryTest.java
+++ collectMe/src/test/java/learn/collectMe/data/CategoryJdbcTemplateRepositoryTest.java
@@ -1,8 +1,12 @@
 package learn.collectMe.data;
 
+import learn.collectMe.models.Category;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,5 +21,13 @@ class CategoryJdbcTemplateRepositoryTest {
 
     @BeforeEach
     void setup() { knownGoodState.set(); }
+
+    @Test
+    void shouldFindCategories() {
+        List<Category> categories = repository.findAll();
+
+        assertNotNull(categories);
+        assertTrue(categories.size() > 0);
+    }
 
 }
