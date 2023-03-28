2870d5d76715052db49a75f59944ccf4ddd31022
Dwight C Kornbluth II <dkornbluth@dev-10.com>
2023-03-28 12:36:00 -0400

    added Category Mapper

--- collectMe/src/main/java/learn/collectMe/data/mappers/CategoryMapper.java
+++ collectMe/src/main/java/learn/collectMe/data/mappers/CategoryMapper.java
@@ -1,4 +1,17 @@
 package learn.collectMe.data.mappers;
 
-public class CategoryMapper {
+import learn.collectMe.models.Category;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CategoryMapper implements RowMapper<Category> {
+    @Override
+    public Category mapRow(ResultSet resultSet, int i) throws SQLException {
+        Category category = new Category();
+        category.setCategoryId(resultSet.getInt("category_id"));
+        category.setCategoryName(resultSet.getString("name"));
+        return category;
+    }
 }
