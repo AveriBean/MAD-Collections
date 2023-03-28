e93a9f683fdc5813f6c35f9efbbce914eccc720e
Dwight C Kornbluth II <dkornbluth@dev-10.com>
2023-03-28 16:05:13 -0400

    added KnownGoodState class in data test

--- collectMe/src/test/java/learn/collectMe/data/CategoryJdbcTemplateRepositoryTest.java
+++ collectMe/src/test/java/learn/collectMe/data/CategoryJdbcTemplateRepositoryTest.java
@@ -1,7 +1,21 @@
 package learn.collectMe.data;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class CategoryJdbcTemplateRepositoryTest {
+
+    @Autowired
+    CategoryJdbcTemplateRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() { knownGoodState.set(); }
 
 }
--- collectMe/src/test/java/learn/collectMe/data/KnownGoodState.java
+++ collectMe/src/test/java/learn/collectMe/data/KnownGoodState.java
@@ -1,2 +1,22 @@
-package learn.collectMe.data;public class KnownGoodState {
+package learn.collectMe.data;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KnownGoodState {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    static boolean hasRun = false;
+
+    void set() {
+        if (!hasRun) {
+            hasRun = true;
+            jdbcTemplate.update("call set_known_good_state();");
+        }
+    }
+
 }
