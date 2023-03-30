a5c0c5fe955ea67e0c91b1ed8b0f72ae80f31894
Dwight C Kornbluth II <dkornbluth@dev-10.com>
2023-03-30 14:23:49 -0400

    added requestMapping to itemcontroller

--- collectMe/src/main/java/learn/collectMe/controllers/CategoryController.java
+++ collectMe/src/main/java/learn/collectMe/controllers/CategoryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @RestController
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin
 @RequestMapping("api/category")
 public class CategoryController {
     private final CategoryService service;
