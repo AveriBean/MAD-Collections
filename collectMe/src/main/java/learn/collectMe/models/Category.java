79371e369d694b22ac56f98f25184f5f00a8ed13
mgharder <mharder@dev-10.com>
2023-03-30 09:43:28 -0500

    equals/hash categories

--- collectMe/src/main/java/learn/collectMe/models/Category.java
+++ collectMe/src/main/java/learn/collectMe/models/Category.java
@@ -2,6 +2,7 @@ package learn.collectMe.models;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Category {
 
@@ -40,5 +41,18 @@ public class Category {
 
     public void setItems(List<Item> items) {
         this.items = items;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return categoryId == category.categoryId && Objects.equals(categoryName, category.categoryName) && Objects.equals(items, category.items);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(categoryId, categoryName, items);
     }
 }
