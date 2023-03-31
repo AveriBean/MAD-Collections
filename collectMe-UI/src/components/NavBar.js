4867fac805d5f1a539280b03bc38667b2ca360cf
Dwight C Kornbluth II <dkornbluth@dev-10.com>
2023-03-31 09:56:49 -0400

    working on NavBar

--- collectMe-UI/src/components/NavBar.js
+++ collectMe-UI/src/components/NavBar.js
@@ -1,29 +1,26 @@
 import { Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar () {
     return (
 
-        <Navbar bg="dark" variant="dark" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home"><h1 className="text-white">Field Agents Inventory</h1></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-navt" />
-                <Navbar.Collapse id="basic-light-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link className="text-secondary" href="/">Home</Nav.Link>
-                    <Nav.Link className="text-secondary" href="/add">Add Agent</Nav.Link>
-                    <Nav.Link className="text-secondary" href="/agents">Agents</Nav.Link>
-                    <Nav.Link className="text-secondary" href="/construction">Agencies</Nav.Link>
-                    <Nav.Link className="text-secondary" href="/construction">Alias</Nav.Link>
-                    <Nav.Link className="text-secondary" href="/construction">Locations</Nav.Link>
-                </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#home"><h1 className="text-white">Field Agents Inventory</h1></a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="collapsible">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        
+                        <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/add">Add Item</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/categories">Categories</Link></li>
+                        <li className="nav-item"><Link className="nav-link" to="/items">Items</Link></li>
 
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
