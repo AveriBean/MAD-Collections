19df83db1ae053f6a4820052e44616e6e4e9dcd6
Dwight C Kornbluth II <dkornbluth@dev-10.com>
2023-03-31 08:25:29 -0400

    update dependencies

--- collectMe-UI/src/App.js
+++ collectMe-UI/src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import AboutUs from "./components/AboutUs";
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+import Footer from "./components/Footer";
+
+
 function App() {
   return (
     <div className="container">
       <Router>
         <NavBar />
+        <SideBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
@@ -10,6 +19,7 @@ function App() {
           <Route path="/items/:id" element={<Item />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
