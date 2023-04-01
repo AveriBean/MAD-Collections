dd83403a88708ce9afec325c62e20aa90acd379d
mgharder <mharder@dev-10.com>
2023-03-31 19:55:17 -0500

    itemForm updates and actionService

--- collectMe-UI/src/App.js
+++ collectMe-UI/src/App.js
@@ -1,27 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home"
+import Home from "./components/Home";
 // import AboutUs from "./components/AboutUs";
-import NavBar from "./components/NavBar"
+import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
 import Footer from "./components/Footer";
-
+import ItemForm from "./components/ItemForm";
 
 function App() {
   return (
-    <div className="container-fluid">
-      <Router>
-        <NavBar />
-        <SideBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {/* <Route path="/about" element={<AboutUs />} /> */}
-          {/* <Route path="/items" element={<Items />} /> */}
-          {/* <Route path="/items/:id" element={<Item />} /> */}
-          {/* <Route path="*" element={<NotFound />} /> */}
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
+    <ItemForm />
+    // <div className="container-fluid">
+    //   <Router>
+    //     <NavBar />
+    //     <SideBar />
+    //     <Routes>
+
+    //       <Route path="/" element={<Home />} />
+    //        <Route path="/about" element={<AboutUs />} />
+    //        <Route path="/items" element={<Items />} />
+    //        <Route path="/items/:id" element={<Item />} />
+    //        <Route path="*" element={<NotFound />} />
+    //     </Routes>
+    //     <Footer />
+    //   </Router>
+    // </div>
   );
 }
 
