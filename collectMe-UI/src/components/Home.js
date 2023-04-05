4cd921addf1b568e1b5c6c7034618876c53f5922
mgharder <mharder@dev-10.com>
2023-04-05 16:48:34 -0500

    updates

--- collectMe-UI/src/components/Home.js
+++ collectMe-UI/src/components/Home.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { findAll, findById } from "../services/itemService";
 
 import Card from "react-bootstrap/Card";
@@ -10,11 +11,11 @@ import bg2 from "../images/carousel/sportcardsresize.jpg";
 import bg3 from "../images/carousel/carsresize.jpg";
 import bg4 from "../images/carousel/nutcrackers.jpg";
 
-function Home () {
-
-    const [items, setItems] = useState([]);
-    const [oneItem, setOneItem] = useState('');
-    const [index, setIndex] = useState(0);
+function Home() {
+  const [items, setItems] = useState([]);
+  const [oneItem, setOneItem] = useState("");
+  const [index, setIndex] = useState(0);
+  const navigate = useNavigate();
 
   useEffect(() => {
     findAll()
@@ -26,8 +27,7 @@ function Home () {
       .catch(alert);
   }, []);
 
-
-    const handleSelect = (selectedIndex, e) => {
+  const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
 
@@ -37,75 +37,95 @@ function Home () {
       style={{ minHeight: "75vh" }}
     >
       <div className="col">
-                <Carousel activeIndex={index} onSelect={handleSelect}>
-                    <Carousel.Item style={{maxHeight: "650px"}}>
-                        <img
-                        className="w-100 h-50"
-                        style={{ objectFit: "cover"  }}
-                        src={bg1}
-                        alt="First slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item style={{maxHeight: "650px"}}>
-                        <img
-                        className="w-100 h-50"
-                        style={{ objectFit: "cover"  }}
-                        src={bg2}
-                        alt="Second slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item style={{maxHeight: "650px"}}>
-                        <img
-                        className="w-100 h-50"
-                        style={{ objectFit: "cover"  }}
-                        src={bg3}
-                        alt="Third slide"
-                        />
-                    </Carousel.Item>
-                    <Carousel.Item style={{maxHeight: "650px"}}>
-                        <img
-                        className="w-100 h-50"
-                        style={{ objectFit: "cover"  }}
-                        src={bg4}
-                        alt="Fourth slide"
-                        />
-                    </Carousel.Item>
-                </Carousel>
-            </div>
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item style={{ maxHeight: "650px" }}>
+            <img
+              className="w-100 h-50"
+              style={{ objectFit: "cover" }}
+              src={bg1}
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item style={{ maxHeight: "650px" }}>
+            <img
+              className="w-100 h-50"
+              style={{ objectFit: "cover" }}
+              src={bg2}
+              alt="Second slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item style={{ maxHeight: "650px" }}>
+            <img
+              className="w-100 h-50"
+              style={{ objectFit: "cover" }}
+              src={bg3}
+              alt="Third slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item style={{ maxHeight: "650px" }}>
+            <img
+              className="w-100 h-50"
+              style={{ objectFit: "cover" }}
+              src={bg4}
+              alt="Fourth slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </div>
 
-            <div className="d-flex-row align-items-center justify-content-center">
-                <h2 className="d-flex justify-content-center">ITEM OF THE DAY</h2>
+      <div className="d-flex-row align-items-center justify-content-center">
+        <h2 className="d-flex justify-content-center">ITEM OF THE DAY</h2>
 
-      <Col className="d-flex justify-content-center">
-        <Card style={{ width: "18rem", marginTop: "10px" }}>
-          <Card.Img
-            style={{ height: "auto", width: "18rem", marginTop: "10px" }}
-            src={oneItem.image}
-              />
-          <Card.Body style={{ textAlign: "center" }}>
-            <Card.Title style={{ marginBottom: "10px" }}>
-              {oneItem.itemName}
-            </Card.Title>
-            <Card.Text><b>Description:</b> {oneItem.description}</Card.Text>
-            <Card.Text><b>Value:</b> {oneItem.value}</Card.Text>
-             <Card.Text>
-                        <b>Item Status:</b>
-                        <div>{oneItem.viewable === true ? "viewable": "" }</div>
-                        <div>{oneItem.tradeable === true ? "tradeable": "" }</div>
-                        <div>{oneItem.saleable === true ? "saleable": "" }</div>
-                        <div>{oneItem.negotiable === true ? "negotiable": "" }</div>
-                    </Card.Text>
-                     <Card.Text>
-                        <b>Categories:</b>
-                        <div>{oneItem.categories && oneItem.categories[0].categoryName}</div>
-                    </Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
+        <Col className="d-flex justify-content-center">
+          <Card style={{ width: "18rem", marginTop: "10px" }}>
+            <Card.Img
+              style={{ height: "auto", width: "18rem", marginTop: "10px" }}
+              src={oneItem.image}
+            />
+            <Card.Body style={{ textAlign: "center" }}>
+              <Card.Title style={{ marginBottom: "10px" }}>
+                {oneItem.itemName}
+              </Card.Title>
+              <Card.Text>
+                <b>Description:</b> {oneItem.description}
+              </Card.Text>
+              <Card.Text>
+                <b>Value:</b> {oneItem.value}
+              </Card.Text>
+              <Card.Text>
+                <b>Item Status:</b>
+                <div>{oneItem.viewable === true ? "viewable" : ""}</div>
+                <div>{oneItem.tradeable === true ? "tradeable" : ""}</div>
+                <div>{oneItem.saleable === true ? "saleable" : ""}</div>
+                <div>{oneItem.negotiable === true ? "negotiable" : ""}</div>
+              </Card.Text>
+              <Card.Text>
+                <b>Categories:</b>
+                <div>
+                  {oneItem.categories && oneItem.categories[0].categoryName}
+                </div>
+              </Card.Text>
+              <Card.Text>
+                <button
+                  onClick={() => navigate(`/view/item/${oneItem.itemId}`)}
+                  style={{
+                    background: "black",
+                    border: "1px solid lightsteelblue",
+                    color: "#D3D3D3",
+                    margin: "5%",
+                    boxShadow: "5px 5px 3px rgba(46, 46, 46, 0.62)",
+                  }}
+                  className="btn btn-info"
+                >
+                  View Item
+                </button>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </div>
     </div>
-  </div>
-
-    );
+  );
 }
 
 export default Home;
